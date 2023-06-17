269a28eac81d3ed3d4c64453cfca3b55c9d59e33
yuni8500 <nurayuni8500@gmail.com>
2023-06-17 15:34:03 +0800

    message

--- app/Console/Commands/SendAppointmentReminder.php
+++ app/Console/Commands/SendAppointmentReminder.php
@@ -32,7 +32,7 @@ class SendAppointmentReminder extends Command
     */
     public function handle(): void
 {
-    $tomorrow = Carbon::now()->addDay()->toDateString();
+    $tomorrow = Carbon::now('UTC')->addDay()->toDateString();
 
     $appointments = Appointment::where('appointDate', $tomorrow)
         ->where('statusAppoint', 'Approved')
